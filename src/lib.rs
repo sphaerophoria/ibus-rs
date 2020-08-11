53c38c45e735d04a51af0b32d2038d22979b0f0b
Mick Sayson <mick@sayson.com>
2020-08-11 15:46:50 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate ibus_sys;
+#[macro_use]
+extern crate glib;
+extern crate glib_sys;
+extern crate gobject_sys;
+extern crate libc;
+extern crate gio;
+extern crate once_cell;
+
+mod auto;
+
+pub use auto::*;
